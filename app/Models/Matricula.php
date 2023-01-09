c59f042d701432bc0e3e3f4d5b747e5eb355dcd3
Cesar Augusto <cezzaar@gmail.com>
2023-01-09 19:14:05 -0300

    Update Matricula.php

--- app/Models/Matricula.php
+++ app/Models/Matricula.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Matricula extends Model
 {
     protected $table = 'imomat';
-    protected $primaryKey = 'imo';
+    protected $primaryKey = 'sr_recno';
     public $incrementing = false;
 
     public function imovel() {
